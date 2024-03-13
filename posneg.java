fca44d2010d14f90a29bb467c0b238b803694711
Soumyasreehalder <90525257+Soumyasreehalder@users.noreply.github.com>
2024-03-13 22:23:13 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,20 @@
+class posneg 
+{
+	public static void main(String[] args) 
+	{
+		int a = -5;
+
+		if (a>0)
+		{
+			System.out.println("It is a Positive number");
+		}
+		else if (a<0)
+		{
+			System.out.println("It is a negetive number");
+		}
+		else 
+		{
+			System.out.println("It is a zero");
+		}
+	}
+}
