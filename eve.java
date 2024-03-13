0c9d9ecfa4c9236703005565940b99030f6a400e
Soumyasreehalder <90525257+Soumyasreehalder@users.noreply.github.com>
2024-03-13 22:30:05 +0530

    Add files via upload

---
+++ eve.java
@@ -0,0 +1,17 @@
+class eve 
+{
+	public static void main(String[] args) 
+	{
+		int a = -5;
+
+		if (a/2==0)
+		{
+			System.out.println("It is a even number");
+		}
+		else if (a/2!=0)
+		{
+			System.out.println("It is a odd number");
+		}
+		
+	}
+}
