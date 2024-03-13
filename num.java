b63afa9cfc0ea6ebfe16b57520c6c16f97c3b87c
Soumyasreehalder <90525257+Soumyasreehalder@users.noreply.github.com>
2024-03-13 22:36:24 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,16 @@
+//Sum of First N Natural numbers:  
+
+class num 
+{
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		int sum =0;
+		for (int i=0;i<=n ;i++ )
+		{
+			sum = sum+i;
+			
+		}
+		System.out.println(sum);
+	}
+}
