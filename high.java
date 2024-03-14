ba346d816914d6bd5ad0f1e6ec654bc2372caa96
Soumyasreehalder <90525257+Soumyasreehalder@users.noreply.github.com>
2024-03-14 19:58:16 +0530

    Add files via upload

---
+++ high.java
@@ -0,0 +1,23 @@
+//Greatest of the Three numbers
+class high
+{
+	public static void main(String[] args) 
+	{
+		int a= 1;
+		int b = 4;
+		int c = 5;
+		if (a>b && a>c)
+		{
+			System.out.println("a is the greatest");
+		}
+		else if (b>a && b>c)
+		{
+			System.out.println("b is the greatest");
+		}
+		else
+		{
+			System.out.println("c is the greatest");
+		}
+		
+}
+}
