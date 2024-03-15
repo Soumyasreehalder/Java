42b8488dfdb80ec70f65324145992b0e0ea9e4d8
Soumyasreehalder <90525257+Soumyasreehalder@users.noreply.github.com>
2024-03-15 23:38:37 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,15 @@
+class circle  
+ 
+{
+	public static void area() 
+	{   double pi = 3.142;
+	    int r = 4;
+		double op = pi*r*r;
+		System.out.println("The area of circle is " + op);
+	}
+
+	public static void main(String[] args) 
+	{
+		area();
+	}
+}
