6370b2f5fb8177fdff2be42768c150a914cba16a
Soumyasreehalder <90525257+Soumyasreehalder@users.noreply.github.com>
2024-03-15 23:39:14 +0530

    Add files via upload

---
+++ rectangle .java
@@ -0,0 +1,16 @@
+class rectangle  
+ 
+{
+	public static void area() 
+	{   
+	    int w = 4;
+		int h = 2;
+		int op = w*h;
+		System.out.println("The area of rectangle is " + op);
+	}
+
+	public static void main(String[] args) 
+	{
+		area();
+	}
+}
