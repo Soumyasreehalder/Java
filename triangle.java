e36b23098eabbd4419ff44b1d2069e46d686da0c
Soumyasreehalder <90525257+Soumyasreehalder@users.noreply.github.com>
2024-03-15 23:40:05 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,15 @@
+class triangle 
+ 
+{
+	public static void area() 
+	{   int b = 2;
+	    int h = 4;
+		double op = 0.5*b*h;
+		System.out.println("The area of triangle is " + op);
+	}
+
+	public static void main(String[] args) 
+	{
+		area();
+	}
+}
